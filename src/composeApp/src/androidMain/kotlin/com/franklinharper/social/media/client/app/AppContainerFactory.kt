b8a406c1a994632193e625fd94e204f620ce0e20
Frank Harper <frank@franklinharper.com>
2026-03-24 08:49:52 -0700

    Fix android compose bootstrap clock call

--- src/composeApp/src/androidMain/kotlin/com/franklinharper/social/media/client/app/AppContainerFactory.kt
+++ src/composeApp/src/androidMain/kotlin/com/franklinharper/social/media/client/app/AppContainerFactory.kt
@@ -5,5 +5,5 @@ import com.franklinharper.social.media.client.db.AndroidDatabaseFactory
 internal actual fun createAppContainer(): AppContainer =
     buildAppContainer(
         database = AndroidDatabaseFactory.createSocialMediaDatabase(),
-        clock = { System.currentTimeMillis() },
+        clock = { java.lang.System.currentTimeMillis() },
     )
